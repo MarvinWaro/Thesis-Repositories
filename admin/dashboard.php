611ba56b737ef4d6da673e46306b364823bb8605
MarvinWaro <waromarvin@gmail.com>
2023-01-22 23:49:20 +0800

    scratch dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,14 +4,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--bootstrap css-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/style.css">
+    <!--bOXiCON cdn-->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>CCS TR | Dashboard</title>
 </head>
 <body>
-    
+    <div class="sidebar">
+        <div class="logo-details">
+            <span class="logo_name">Thesis Repositories</span>
+        </div>
+
+        <hr class="line">
+
+        <ul class="nav-links">
+            <li>
+                <a href="#">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="link-name">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-folder-open'></i>
+                    <span class="link-name">Archives</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-task'></i>
+                    <span class="link-name">Thesis Status</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-user'></i>
+                    <span class="link-name">Manage Student</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-group' ></i>
+                    <span class="link-name">Manage Panelist</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-group' ></i>
+                    <span class="link-name">Manage Adviser</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-calendar' ></i>
+                    <span class="link-name">Managae Schedule</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-note'></i>
+                    <span class="link-name">Manage Rubricks</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <!--Homecontent-->
+
+    <section class="home-section">
+        <nav>
+            <div class="sidebar-menu">
+                <i class='bx bx-menu'></i>
+            </div>
+        </nav>
+    </section>
+
 </body>
 </html>
