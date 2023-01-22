e41e7617bd25fafe829267f22d9b4d6df3f544da
MarvinWaro <waromarvin@gmail.com>
2023-01-22 13:42:32 +0800

    hmm

---
+++ landingpage/landingpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<section class="header">
+    <div class="container">
+
+    
+    <div class="middle">
+        <h1>Welcom to</h1>
+    </div>
+
+    </div>
+</section>
+
+</body>
+</html>
