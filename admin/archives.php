d188d5317144dfee7c698e7ad9991f0979b5b9b1
MarvinWaro <waromarvin@gmail.com>
2023-02-04 06:45:36 +0800

    ...

--- admin/archives.php
+++ admin/archives.php
@@ -229,14 +229,14 @@
     </section>
 
     <script src="../js/script.js"></script>
-    <!--<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function () {
             $('#example').DataTable();
         });
-    </script>-->
+    </script>
 
 </body>
 </html>
