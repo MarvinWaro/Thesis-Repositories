16c797201f3c14f92f5f1ace13d805bdd3707b39
MarvinWaro <waromarvin@gmail.com>
2023-02-05 23:26:26 +0800

    Update manage_students.php

--- admin/manage_students.php
+++ admin/manage_students.php
@@ -273,5 +273,6 @@ if (!isset($_SESSION['logged-in'])){
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" ></script>
 </body>
 </html>
