d02b49bb413f66ce857fba8ee2784b655335dc64
Schyann Schleiden Kenoh <shanopry@gmail.com>
2023-02-21 09:38:36 +0800

    sidebar

--- faculty/panel/home.php
+++ faculty/panel/home.php
@@ -1,3 +1,19 @@
+<?php
+
+
+        session_start();
+        /*
+            if user is not login then redirect to login page,
+            this is to prevent users from accessing pages that requires
+            authentication such as the dashboard
+        */
+        if (!isset($_SESSION['logged-in'])){
+            header('location: ../login/login.php');
+        }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -33,7 +49,7 @@
       </div>
       <ul class="sidebar-menu p-3 m-0 mb-0">
         <li class="sidebar-menu-item active">
-          <a href="#">
+          <a href="dashboard.php">
             <i class="ri-dashboard-line sidebar-menu-item-icon"></i>
             Dashboard
           </a>
@@ -95,3 +111,15 @@
     </div>
     <div class="sidebar-overlay"></div>
     <!-- end: Sidebar -->
+    <script src="../assets/js/jquery.min.js"></script>
+    <script>
+      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js"
+      integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    </script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/script.js"></script>
+    <!-- end: JS -->
+  </body>
+</html>
