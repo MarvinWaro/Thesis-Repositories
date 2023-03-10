12945de9f326920a6820aeea399e65ede3754933
MarvinWaro <waromarvin@gmail.com>
2023-03-10 12:27:00 +0800

    added: add student form / edit form

---
+++ admin/add_student.php
@@ -0,0 +1,353 @@
+
+    <?php
+
+require_once '../class/database.php';
+require_once '../class/student.class.php';
+
+session_start();
+/*
+    if user is not login then redirect to login page,
+    this is to prevent users from accessing pages that requires
+    authentication such as the dashboard
+*/
+if (!isset($_SESSION['logged-in'])){
+    header('location: ../login/login.php');
+}
+
+if(isset($_POST['save'])){
+
+    $student = new Student();
+    //sanitize user inputs
+    $student->firstname = htmlentities($_POST['firstname']);
+    $student->middle_name = htmlentities($_POST['middle_name']);
+    $student->lastname = htmlentities($_POST['lastname']);
+    $student->username = htmlentities($_POST['username']);
+    $student->email = htmlentities($_POST['email']);
+    $student->password = htmlentities($_POST['password']);
+    $student->course = htmlentities($_POST['course']);
+    $student->year_and_section = htmlentities($_POST['year_and_section']);
+    $student->sem = htmlentities($_POST['sem']);
+    $student->school_year = htmlentities($_POST['school_year']);
+    $student->your_adviser = htmlentities($_POST['your_adviser']);
+    $student->your_group = htmlentities($_POST['your_group']);
+    $student->type = $_POST['type'];
+
+    if(isset($_POST)){
+        if($student->add()){
+            //redirect user to create page after saving
+            header('location: manage_students.php');
+        }
+    }
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!-- start: Icons -->
+<link
+  href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
+  rel="stylesheet"
+/>
+<!-- start: Icons -->
+<!-- start: CSS -->
+<link rel="icon" href="../img/rlogo.png" type="image/icon type">
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../assets/css/style.css" />
+<!-- end: CSS -->
+<title>Thesis Repository - Blank Page</title>
+</head>
+
+<body>
+<!-- start: Sidebar -->
+<div class="sidebar position-fixed top-0 bottom-0 bg-white border-end">
+  <div class="d-flex align-items-center p-3">
+    <a
+      href="#"
+      class="sidebar-logo text-uppercase fw-bold text-decoration-none text-indigo fs-4"
+      >Thesis Repository</a
+    >
+    <i
+      class="sidebar-toggle ri-arrow-left-circle-line ms-auto fs-5 d-none d-md-block"
+    ></i>
+  </div>
+  <ul class="sidebar-menu p-3 m-0 mb-0">
+    <li class="sidebar-menu-item ">
+      <a href="dashboard.php">
+        <i class="ri-dashboard-line sidebar-menu-item-icon"></i>
+        Dashboard
+      </a>
+    </li>
+    <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Thesis</li>
+    <li class="sidebar-menu-item">
+      <a href="archives.php">
+        <i class="ri-archive-drawer-line sidebar-menu-item-icon"></i>
+        Archives
+      </a>
+    </li>
+    <li class="sidebar-menu-item has-dropdown">
+      <a href="thesis_status.php">
+          <i class="ri-bar-chart-box-line sidebar-menu-item-icon"></i>
+          Thesis Status
+          <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
+      </a>
+      <ul class="sidebar-dropdown-menu">
+          <li class="sidebar-dropdown-menu-item">
+              <a href="accepted.php">
+                  Accepted Titles
+              </a>
+          </li>
+          <li class="sidebar-dropdown-menu-item">
+              <a href="rejected.php">
+                  Rejected Titles
+              </a>
+          </li>
+      </ul>
+  </li>
+
+    <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Manage</li>
+
+    <li class="sidebar-menu-item active">
+      <a href="manage_students.php">
+        <i class="ri-user-line sidebar-menu-item-icon"></i>
+        Manage Student
+      </a>
+    </li>
+    <li class="sidebar-menu-item">
+      <a href="manage_faculty.php">
+        <i class="ri-group-line sidebar-menu-item-icon"></i>
+        Manage Faculty
+      </a>
+    </li>
+    <li class="sidebar-menu-item">
+      <a href="manage_rubrics.php">
+        <i class="ri-table-2 sidebar-menu-item-icon"></i>
+        Manage Rubrics
+      </a>
+    </li>
+    <li class="sidebar-menu-item">
+      <a href="manage_schedules.php">
+        <i class="ri-calendar-2-line sidebar-menu-item-icon"></i>
+        Schedules
+      </a>
+    </li>
+  </ul>
+</div>
+<div class="sidebar-overlay"></div>
+<!-- end: Sidebar -->
+
+<!-- start: Main -->
+<main class="bg-light">
+  <div class="p-2">
+    <!-- start: Navbar -->
+    <nav class="px-3 py-2 bg-white rounded shadow-sm">
+      <i class="ri-menu-line sidebar-toggle me-3 d-block d-md-none"></i>
+      <h5 class="fw-bold mb-0 me-auto">Student</h5>
+      <div class="dropdown me-3 d-none d-sm-block">
+        <div
+          class="cursor-pointer dropdown-toggle navbar-link"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="ri-notification-line"></i>
+        </div>
+        <div class="dropdown-menu fx-dropdown-menu">
+          <h5 class="p-3 bg-indigo text-light">Notification</h5>
+          <div class="list-group list-group-flush">
+            <a
+              href="#"
+              class="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            >
+              <div class="me-auto">
+                <div class="fw-semibold">Subheading</div>
+                <span class="fs-7">Content for list item</span>
+              </div>
+              <span class="badge bg-primary rounded-pill">14</span>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            >
+              <div class="me-auto">
+                <div class="fw-semibold">Subheading</div>
+                <span class="fs-7">Content for list item</span>
+              </div>
+              <span class="badge bg-primary rounded-pill">14</span>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            >
+              <div class="me-auto">
+                <div class="fw-semibold">Subheading</div>
+                <span class="fs-7">Content for list item</span>
+              </div>
+              <span class="badge bg-primary rounded-pill">14</span>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            >
+              <div class="me-auto">
+                <div class="fw-semibold">Subheading</div>
+                <span class="fs-7">Content for list item</span>
+              </div>
+              <span class="badge bg-primary rounded-pill">14</span>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action d-flex justify-content-between align-items-start"
+            >
+              <div class="me-auto">
+                <div class="fw-semibold">Subheading</div>
+                <span class="fs-7">Content for list item</span>
+              </div>
+              <span class="badge bg-primary rounded-pill">14</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="dropdown">
+        <div
+          class="d-flex align-items-center cursor-pointer dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <span class="me-2 d-none d-sm-block"><?php echo $_SESSION['fullname'] ?></span>
+          <img
+            class="navbar-profile-image"
+            src="../img/u-icon.png" alt="Image"
+          />
+        </div>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li><a class="dropdown-item" href="#">Profile</a></li>
+          <li><a class="dropdown-item" href="../login/logout.php">Logout </a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- end: Navbar -->
+
+    <!-- start: Content -->
+    <div class="py-4">
+      <!-- start: Summary -->
+       <div class="main-content border">
+          <div class="form-cont">
+          <form class="edit-form" action="add_student.php" method="post">
+
+                <div class="d-flex justify-content-between pt-2 pb-3">
+                    <h3>Add Student</h3>
+                    <a href="manage_students.php"><i class="ri-arrow-go-back-line back pb-2"></i></a>
+                </div>
+
+                <div class="cont">
+                <label for="firstname">First Name</label>
+                <input class="form-input" type="text" id="firstname" name="firstname" placeholder="Enter First name*" required value="<?php if(isset($_POST['firstname'])) { echo $_POST['firstname']; } ?>">
+                <label for="middle_name">Middle Name</label>
+                <input class="form-input" type="text" id="middle_name" name="middle_name" placeholder="Enter Middle name (optional)*" value="<?php if(isset($_POST['middle_name'])) { echo $_POST['middle_name']; } ?>">
+                </div>
+
+                <div class="cont">
+                <label for="lastname">Last Name</label>
+                <input class="form-input " type="text" id="lastname" name="lastname" placeholder="Enter Last name*" required value="<?php if(isset($_POST['lastname'])) { echo $_POST['lastname']; } ?>">
+                <label for="username">Username</label>
+                <input class="form-input" type="text" id="username" name="username" placeholder="Enter Username*" required value="<?php if(isset($_POST['username'])) { echo $_POST['username']; } ?>">
+                </div>
+
+                <div class="cont">
+                <label for="email">Email</label>
+                <input class="form-input " type="email" id="email" name="email" placeholder="Enter Email*" required value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
+                <label for="password">Password</label>
+                <input class="form-input" type="password" id="password" name="password" placeholder="Enter password" required value="<?php if(isset($_POST['password'])) { echo $_POST['password']; } ?>">
+                </div>
+
+
+                <label for="course">Course</label>
+                <select name="course" id="course">
+                    <option value="none <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'None') echo ' selected="selected"'; } ?>">--Select Course--</option>
+                    <option value="BSIT" <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'BSIT') echo ' selected="selected"'; } ?>>BSIT</option>
+                    <option value="BSCS" <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'BSIT') echo ' selected="selected"'; } ?>>BSCS</option>
+                </select>
+
+
+
+
+                <label for="year_and_section">Year & Section</label>
+                <select name="year_and_section" id="year_and_section">
+                    <option value="none <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == 'None') echo ' selected="selected"'; } ?>">--Select Year & Section--</option>
+                    <option value="3-A" <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == '3-A') echo ' selected="selected"'; } ?>>3-A</option>
+                    <option value="3-B" <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == '3-B') echo ' selected="selected"'; } ?>>3-B</option>
+                    <option value="3-C" <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == '3-C') echo ' selected="selected"'; } ?>>3-C</option>
+                    <option value="4-A" <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == '4-A') echo ' selected="selected"'; } ?>>4-A</option>
+                    <option value="4-B" <?php if(isset($_POST['year_and_section'])) { if ($_POST['year_and_section'] == '4-B') echo ' selected="selected"'; } ?>>4-B</option>
+                </select>
+
+
+                <label for="sem">Semester</label>
+                <select name="sem" id="sem">
+                    <option value="none" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'None') echo ' selected="selected"'; } ?>>--Select Semester--</option>
+                    <option value="First Semester" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'fisrt_sem') echo ' selected="selected"'; } ?>>First Semester</option>
+                    <option value="Second Semester" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'second_sem') echo ' selected="selected"'; } ?>>Second Semester</option>
+                </select>
+
+
+                <label for="your_adviser">Adviser</label>
+                <select name="your_adviser" id="your_adviser">
+                    <option value="Jaydee Ballaho" <?php if(isset($_POST['your_adviser'])) { if ($_POST['your_adviser'] == 'Jaydee Ballaho') echo ' selected="selected"'; } ?>>Jaydee Ballaho</option>
+                    <option value="Marjorie Rojas" <?php if(isset($_POST['your_adviser'])) { if ($_POST['your_adviser'] == 'Marjorie Rojas') echo ' selected="selected"'; } ?>>Marjorie Rojas</option>
+                </select>
+
+                <label for="your_group">Group no.</label>
+                <select name="your_group" id="your_group">
+                    <option value="1" <?php if(isset($_POST['your_group'])) { if ($_POST['your_group'] == '1') echo ' selected="selected"'; } ?>>Group 1</option>
+                </select>
+
+                <label for="school_year">School Year</label>
+                <select name="school_year" id="school_year">
+                    <option value="2022-2023" <?php if(isset($_POST['school_year'])) { if ($_POST['school_year'] == 'current_sy') echo ' selected="selected"'; } ?>>Current SY</option>
+                </select>
+
+
+
+                <label for="type">Regular?</label>
+                <input type="radio" name="type" id="student" value="student" <?php if(isset($_POST['type'])) { if ($_POST['type'] == 'student') echo ' checked'; } ?>>
+                <label for="type">Irregular?</label>
+                <input type="radio" name="type" id="student" value="student" <?php if(isset($_POST['type'])) { if ($_POST['type'] == 'student') echo ' checked'; } ?>>
+
+                    <div class="pbutton">
+                        <input class="save-btn form-input" type="submit" value="Save" name="save">
+                    </div>
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end: Summary -->
+      <!-- start: Graph -->
+
+      <!-- end: Graph -->
+    </div>
+    <!-- end: Content -->
+  </div>
+</main>
+<!-- end: Main -->
+
+<!-- start: JS -->
+<script src="../assets/js/jquery.min.js"></script>
+<script
+  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js"
+  integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/script.js"></script>
+<!-- end: JS -->
+</body>
+</html>
--- admin/manage_students.php
+++ admin/manage_students.php
@@ -14,34 +14,6 @@ if (!isset($_SESSION['logged-in'])){
 }
 
 
-if(isset($_POST['save'])){
-
-    $student = new Student();
-    //sanitize user inputs
-    $student->firstname = htmlentities($_POST['firstname']);
-    $student->middle_name = htmlentities($_POST['middle_name']);
-    $student->lastname = htmlentities($_POST['lastname']);
-    $student->username = htmlentities($_POST['username']);
-    $student->email = htmlentities($_POST['email']);
-    $student->password = htmlentities($_POST['password']);
-    $student->course = htmlentities($_POST['course']);
-    $student->year_level = htmlentities($_POST['year_level']);
-    $student->section = htmlentities($_POST['section']);
-    $student->sem = htmlentities($_POST['sem']);
-    $student->school_year = htmlentities($_POST['school_year']);
-    $student->your_adviser = htmlentities($_POST['your_adviser']);
-    $student->your_group = htmlentities($_POST['your_group']);
-    $student->type = $_POST['type'];
-
-    if(isset($_POST)){
-        if($student->add()){
-            //redirect user to create page after saving
-            header('location: manage_students.php');
-        }
-    }
-}
-
-
 ?>
 
 
@@ -242,9 +214,7 @@ if(isset($_POST['save'])){
           <!-- start: content -->
           <div class="container padding-bottom">
                   <div class="head-cont d-flex justify-content-end pb-2">
-                      <button type="button" class="btn btn-primary add-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                          Add new Student
-                      </button>
+                      <a class="btn btn-primary add-button" href="add_student.php">Add new Student</a>
                   </div>
 
                     <table id="example" class="table table-striped" style="width:100%">
@@ -312,103 +282,6 @@ if(isset($_POST['save'])){
 
         <!--Modal 1-->
 
-            <div class="modal fade>" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Add Student</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-
-                        <div class="modal-body">
-                            <form class="add-form" action="manage_students.php" method="post">
-
-
-                                    <div class="cont">
-                                    <input class="form-input me-2" type="text" id="firstname" name="firstname" placeholder="Enter First name*" required value="<?php if(isset($_POST['firstname'])) { echo $_POST['firstname']; } ?>">
-                                    <input class="form-input" type="text" id="middle_name" name="middle_name" placeholder="Enter Middle name (optional)*" value="<?php if(isset($_POST['middle_name'])) { echo $_POST['middle_name']; } ?>">
-                                    </div>
-
-                                    <div class="cont">
-                                    <input class="form-input me-2" type="text" id="lastname" name="lastname" placeholder="Enter Last name*" required value="<?php if(isset($_POST['lastname'])) { echo $_POST['lastname']; } ?>">
-                                    <input class="form-input" type="text" id="username" name="username" placeholder="Enter Username*" required value="<?php if(isset($_POST['username'])) { echo $_POST['username']; } ?>">
-                                    </div>
-
-                                    <div class="cont">
-                                    <input class="form-input me-2" type="email" id="email" name="email" placeholder="Enter Email*" required value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
-                                    <input class="form-input" type="password" id="password" name="password" placeholder="Enter password" required value="<?php if(isset($_POST['password'])) { echo $_POST['password']; } ?>">
-                                    </div>
-
-
-
-                                    <select name="course" id="course">
-                                        <option value="none <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'None') echo ' selected="selected"'; } ?>">--Select Course--</option>
-                                        <option value="BSIT" <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'BSCS') echo ' selected="selected"'; } ?>>BSCS</option>
-                                        <option value="BSCS" <?php if(isset($_POST['course'])) { if ($_POST['course'] == 'BSIT') echo ' selected="selected"'; } ?>>BSIT</option>
-                                    </select>
-
-
-
-
-                                    <select name="year_level" id="year_level">
-                                        <option value="none" <?php if(isset($_POST['year_level'])) { if ($_POST['year_level'] == 'None') echo ' selected="selected"'; } ?>>--Select Year Level--</option>
-                                        <option value="3rd Year" <?php if(isset($_POST['year_level'])) { if ($_POST['year_level'] == '3rd_year') echo ' selected="selected"'; } ?>>3rd Year</option>
-                                        <option value="4th Year" <?php if(isset($_POST['year_level'])) { if ($_POST['year_level'] == '4th_year') echo ' selected="selected"'; } ?>>4th Year</option>
-                                    </select>
-
-
-
-
-                                    <select name="section" id="section">
-                                        <option value="none"  <?php if(isset($_POST['section'])) { if ($_POST['section'] == 'None') echo ' selected="selected"'; } ?>>--Select Section--</option>
-                                        <option value="A"  <?php if(isset($_POST['section'])) { if ($_POST['section'] == 'A') echo ' selected="selected"'; } ?>>A</option>
-                                        <option value="B"  <?php if(isset($_POST['section'])) { if ($_POST['section'] == 'B') echo ' selected="selected"'; } ?>>B</option>
-                                        <option value="C"  <?php if(isset($_POST['section'])) { if ($_POST['section'] == 'C') echo ' selected="selected"'; } ?>>C</option>
-                                    </select>
-
-
-
-
-                                    <select name="sem" id="sem">
-                                        <option value="none" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'None') echo ' selected="selected"'; } ?>>--Select Semester--</option>
-                                        <option value="First Semester" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'fisrt_sem') echo ' selected="selected"'; } ?>>First Semester</option>
-                                        <option value="Second Semester" <?php if(isset($_POST['sem'])) { if ($_POST['sem'] == 'second_sem') echo ' selected="selected"'; } ?>>Second Semester</option>
-                                    </select>
-
-
-                                    <select name="school_year" id="school_year">
-                                      <option value="2022-2023" <?php if(isset($_POST['school_year'])) { if ($_POST['school_year'] == 'current_sy') echo ' selected="selected"'; } ?>>Current SY</option>
-                                    </select>
-
-                                    <select name="your_adviser" id="your_adviser">
-                                      <option value="Jaydee_Ballaho" <?php if(isset($_POST['your_adviser'])) { if ($_POST['your_adviser'] == 'jaydee') echo ' selected="selected"'; } ?>>Jaydee</option>
-                                    </select>
-
-                                    <select name="your_group" id="your_group">
-                                      <option value="group_1" <?php if(isset($_POST['your_group'])) { if ($_POST['your_group'] == 'group1') echo ' selected="selected"'; } ?>>Group 1</option>
-                                    </select>
-
-
-
-                                    <label for="type">Regular?</label>
-                                    <input type="radio" name="type" id="student" value="student" <?php if(isset($_POST['type'])) { if ($_POST['type'] == 'student') echo ' checked'; } ?>>
-                                    <label for="type">Irregular?</label>
-                                    <input type="radio" name="type" id="student" value="student" <?php if(isset($_POST['type'])) { if ($_POST['type'] == 'student') echo ' checked'; } ?>>
-
-
-                                </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <!--<button type="submit" value="Save" name="save" class="btn btn-primary">Save changes</button>-->
-                                            <input class="button form-input" type="submit" value="Save" name="save">
-                                        </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-
-            </div>
 
             <!--Modal 2-->
     </div>
