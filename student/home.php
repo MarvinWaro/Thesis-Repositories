f0cfbefede776e9f194db2bd4631441c8b75a906
MarvinWaro <waromarvin@gmail.com>
2023-03-17 06:25:20 +0800

    calendar home

--- student/home.php
+++ student/home.php
@@ -33,6 +33,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="../assets/css/style.css" />
+    <link rel="stylesheet" href="../assets/css/calendar.css" />
     <!-- end: CSS -->
     <title>Thesis Repository - Blank Page</title>
   </head>
@@ -178,7 +179,79 @@
         <div class="py-4">
           <!-- start: content -->
 
+
+          <div class="container-calendar">
+            <div class="left">
+            <div class="calendar">
+                <div class="month">
+                <i class="fas fa-angle-left prev"></i>
+                <div class="date">december 2015</div>
+                <i class="fas fa-angle-right next"></i>
+                </div>
+                <div class="weekdays">
+                <div>Sun</div>
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                </div>
+                <div class="days"></div>
+                <div class="goto-today">
+                <div class="goto">
+                    <input type="text" placeholder="mm/yyyy" class="date-input" />
+                    <button class="goto-btn">Go</button>
+                </div>
+                <button class="today-btn">Today</button>
+                </div>
+            </div>
+            </div>
+            <div class="right">
+            <div class="today-date">
+                <div class="event-day">wed</div>
+                <div class="event-date">12th december 2022</div>
+            </div>
             
+            <div class="events"></div>
+            <?php
+                if($_SESSION['user_type'] == 'admin'){ 
+            ?>
+            <div class="add-event-wrapper">
+                <div class="add-event-header">
+                <div class="title">Add Event</div>
+                <i class="fas fa-times close"></i>
+                </div>
+                <div class="add-event-body">
+                <div class="add-event-input">
+                    <input type="text" placeholder="Event Name" class="event-name" />
+                </div>
+                <div class="add-event-input">
+                    <input
+                    type="text"
+                    placeholder="Event Time From"
+                    class="event-time-from"
+                    />
+                </div>
+                <div class="add-event-input">
+                    <input
+                    type="text"
+                    placeholder="Event Time To"
+                    class="event-time-to"
+                    />
+                </div>
+                </div>
+                <div class="add-event-footer">
+                <button class="add-event-btn">Add Event</button>
+                </div>
+            </div>
+            </div>
+            <button class="add-event">
+            <i class="fas fa-plus"></i>
+            </button>
+            <?php
+                }
+            ?>
 
         </div>
 
